b53f96126a087abb3e1cd89bfc3dbc4339bfe4a8
maddu bhavani <bhavani@calibrage.in>
2020-01-29 11:51:02 -0500

    comitted changes

--- QuickApp/ClientApp/src/app/admin/user-editor/user-editor.component.ts
+++ QuickApp/ClientApp/src/app/admin/user-editor/user-editor.component.ts
@@ -42,7 +42,7 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
   @Input() user: any = new User();
   @Input() roles: Role[] = [];
   userFileData: any[] = [];
-  editUserDocs:any[]=[];
+  editUserDocs: any[] = [];
   isEditMode: boolean;
   BASE64_MARKER: string = ';base64,';
   fileExtension: any;
@@ -121,6 +121,7 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
     private ngZone: NgZone
   ) {
     this.buildForm();
+    this.docData();
 
 
   }
@@ -147,11 +148,9 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
     }
 
     this.setRoles();
-
     this.resetForm();
-    this.docData();
     this.getCurrentUserFiles();
-   
+
   }
 
   ngOnDestroy() {
@@ -172,9 +171,10 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
     this.accountService.getCddmtData(1).subscribe(res => {
       this.pDocdata = res;
       this.userDoc = this.pDocdata.listResult;
-      this.editUserDocs =JSON.parse(JSON.stringify(this.userDoc))
+      //console.log(this.userDoc);
+      this.editUserDocs = JSON.parse(JSON.stringify(this.userDoc))
 
-     // this.editUserDocs=this.userDoc;
+      // this.editUserDocs=this.userDoc;
     })
   }
 
@@ -257,7 +257,7 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
   //  File  Change Event
   uploadFile(doc, event) {
     debugger
-    var doc1=doc;
+    var doc1 = doc;
     var file = event.target.files[0];
     if (doc.typeCdDmtId == DataFactory.TypeOfFile.Image) {
       var extensions = (this.allowedImageExtension.split(',')).map(function (x) { return x.toLocaleUpperCase().trim() });
@@ -323,12 +323,12 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
           "fileName": e.target.result.substring(base64Index),
           "fileLocation": null,
           "fileExtention": this.fileExtension,
-          "documentTypeId":doc1.typeCdDmtId,
+          "documentTypeId": doc1.typeCdDmtId,
           "createdBy": this.currentUser.id,
           "updatedBy": this.currentUser.id,
           "updatedDate": new Date(),
           "createdDate": new Date(),
-          
+
         })
     }
     reader.readAsDataURL(file);
@@ -367,7 +367,7 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
 
 
     if (this.isNewUser) {
-   
+
       this.accountService.newUser(editedUser).subscribe(
         user => this.saveCompleted(user),
         error => this.saveFailed(error));
@@ -535,9 +535,10 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
 
   // Current User Files
   getCurrentUserFiles() {
-    debugger
-    this.editUserFilesList = this.editUserDocs;
+    this.editUserFilesList = [];
+    console.log(this.editUserDocs)
     this.accountService.getUserFileData(this.user.id).subscribe(res => {
+      this.editUserFilesList = JSON.parse(JSON.stringify(this.userDoc));
       this.userFileInfo = res;
       this.userFileData = this.userFileInfo;
       this.editUserFilesList.forEach((item) => {
@@ -546,6 +547,7 @@ export class UserEditorComponent implements OnChanges, OnDestroy {
         });
       });
     })
+
   }
 
   //  On Delete File
