43ffb29c70bae9a12897f5c52067f13f5afbaba9
DESKTOP-RJHPVF7\Calibrage24 <srrajeswari55@gmail.com>
2020-01-30 10:40:54 +0530

    added chnages

--- DAL/RequestResponseModels/RequestResponseModels.cs
+++ DAL/RequestResponseModels/RequestResponseModels.cs
@@ -21,6 +21,7 @@ namespace DAL.RequestResponseModels
             public string Address { get; set; }
             public double Latitude { get; set; }
             public double Longitude { get; set; }
+            public bool IsActive { get; set; }
         }
 
         public class UpsertRepository : AuditableEntity
