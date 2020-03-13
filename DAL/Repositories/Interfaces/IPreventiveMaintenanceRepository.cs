1352828d2817473cc78b177e2813dfb765ec7b93
DESKTOP-RJHPVF7\Calibrage24 <srrajeswari55@gmail.com>
2020-03-13 11:54:04 +0530

    Added Preventive APIs

---
+++ DAL/Repositories/Interfaces/IPreventiveMaintenanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+using DAL.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static DAL.RequestResponseModels.RequestResponseModels;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IPreventiveMaintenanceRepository
+    {
+        ListDataResponse<GetPreventiveMaintenanceResponse> GetAllPreventiveMaintenances();
+
+        ValueDataResponse<PreventiveMaintenance> InsertPreventiveMaintenance(UpsertPreventiveMaintenance PmOrder);
+
+        ValueDataResponse<PreventiveMaintenance> UpdatePreventiveMaintenance(UpsertPreventiveMaintenance PmOrder);
+    }
+}
