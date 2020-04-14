4cb1e96789273e93d35d0f4b87308c68b6670605
Amith Si <amith@calibrage.in>
2020-04-14 10:37:06 +0530

    commited changes for new screen Charts

---
+++ QuickApp/ClientApp/src/app/admin/charts/charts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-charts',
+  templateUrl: './charts.component.html',
+  styleUrls: ['./charts.component.scss']
+})
+export class ChartsComponent implements OnInit {
+
+  constructor() { }
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels = ['2014', '2015', '2016', '2017', '2018', '2019', '2020'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+  ngOnInit() {
+  }
+
+}
