00e7e70ef64b0d0dacc14b23773ae5353c71132c
Rajeswari <srrajeswari55@gmail.com>
2020-05-23 10:13:38 +0530

    added chnages

--- QuickApp/ClientApp/src/environments/environment.prod.ts
+++ QuickApp/ClientApp/src/environments/environment.prod.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
   production: true,
-  baseUrl: 'http://183.82.111.111/MMSAPI', 
+ // baseUrl: 'http://183.82.111.111/MMSAPI', 
   //baseUrl: 'http://183.82.111.111/MMSTestAPI', 
- // baseUrl: 'http://localhost/MMSAPI',// Change this to the address of your backend API if different from frontend address
+  baseUrl: 'http://192.168.129.173/MMSAPI',// Change this to the address of your backend API if different from frontend address
   tokenUrl: null, // For IdentityServer/Authorization Server API. You can set to null if same as baseUrl
   loginUrl: '/login'
 };
